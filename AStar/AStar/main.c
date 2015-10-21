d316cb3c4a9a84bac9b63e73c4a186278646acef
Luis de Jesus Martin Castillo <you@example.com>
2015-10-21 01:54:15 -0500

    Se crea el proyecto

---
+++ AStar/AStar/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AStar
+//
+//  Created by Luis de Jesus Martin Castillo on 21/10/15.
+//  Copyright © 2015 Luis de Jesus Martin Castillo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
